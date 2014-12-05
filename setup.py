0fb9d01a2c2ab20edb934ebd5a0f2fda63934233
Morgan <morgan.bohn@unistra.fr>
2014-12-05 16:41:01 +0100

    modif setup

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ recursive_requirements('requirements.txt', libraries, dependency_links)
 
 setup(
     name='django-hypnos',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=libraries,
     dependency_links=dependency_links,
