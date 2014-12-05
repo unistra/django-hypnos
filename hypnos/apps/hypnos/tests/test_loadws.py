ba3bbe328da3ec3b5759668f85ea9e01938cbd89
Morgan <morgan.bohn@unistra.fr>
2014-12-05 19:36:20 +0100

    add tearDown for test

--- hypnos/apps/hypnos/tests/test_loadws.py
+++ hypnos/apps/hypnos/tests/test_loadws.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.management import call_command
 from os.path import exists, join
+from os import remove
 from django.conf import settings
 
 class LoadWebserviceTest(TestCase):
@@ -19,3 +20,14 @@ class LoadWebserviceTest(TestCase):
         self.assertTrue(exists(join(self.app_folder, "views.py")))
         self.assertTrue(exists(join(self.app_folder, "urls.py")))
         self.assertTrue(exists(join(self.app_folder, "filters.py")))
+
+    def tearDown(self):
+        try:
+            remove(join(self.app_folder, "__init__.pyc"))
+            remove(join(self.app_folder, "models.pyc"))
+            remove(join(self.app_folder, "serializers.pyc"))
+            remove(join(self.app_folder, "views.pyc"))
+            remove(join(self.app_folder, "urls.pyc"))
+            remove(join(self.app_folder, "filters.pyc"))
+        except:
+            pass
